e00d8ff1c1c92a6c453480b52a8b04357bf80a38
sojicute <sojicute137@gmail.com>
2022-05-18 22:32:12 +0400

    git init spring project

---
+++ src/main/java/com/sojicute/petprojectdemo/PetProjectDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sojicute.petprojectdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetProjectDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetProjectDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sojicute/petprojectdemo/PetProjectDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sojicute.petprojectdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetProjectDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
